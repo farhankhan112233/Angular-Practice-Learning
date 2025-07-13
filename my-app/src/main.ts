d71356cd8dc4b64db1f1c2efa7c8a044a59dc5d5
Farhan Khan <khanjanfarhan2@gmail.com>
2025-07-13 18:28:48 +0500

    Pipi Branch code comit line

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -4,3 +4,4 @@ import { appConfig } from './app/app.config';
 import { App } from './app/AppComponent/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+//bootstrapApplication(App, appConfig).catch((err) => console.error(err));
